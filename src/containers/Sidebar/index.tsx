d2d6aea9d8091a4f29ad226ddbd62a729056072c
Leonardo Palheiros <leonardopalheiros@hotmail.com>
2025-05-15 13:35:13 -0300

    estilizando a sidebar

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,18 @@
+import Avatar from "../../components/Avatar"
+import Paragrafo from "../../components/Paragrafo"
 import Titulo from "../../components/Titulo"
+import { Descricao, BotaoTema, SidebarContainer } from "./styles"
+
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/LeoPalheiros.png" />
-    <Titulo fontSize={20}>Leonardo Palheiros</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Leonardo Palheiros</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>inclemencia</Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>Engenheiro Front-End</Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
