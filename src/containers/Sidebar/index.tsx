1d1e18851e5f427b2785f88a2817f4b723e2a749
Leonardo Palheiros <leonardopalheiros@hotmail.com>
2025-05-15 14:15:02 -0300

    finalizando o theme

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -3,15 +3,18 @@ import Paragrafo from "../../components/Paragrafo"
 import Titulo from "../../components/Titulo"
 import { Descricao, BotaoTema, SidebarContainer } from "./styles"
 
+type Props = {
+  trocaTema: () => void;
+}
 
-const Sidebar = () => (
+const Sidebar = (props: Props) => (
   <aside>
     <SidebarContainer>
       <Avatar />
       <Titulo fontSize={20}>Leonardo Palheiros</Titulo>
       <Paragrafo tipo="secundario" fontSize={16}>inclemencia</Paragrafo>
       <Descricao tipo="principal" fontSize={12}>Engenheiro Front-End</Descricao>
-      <BotaoTema>Trocar tema</BotaoTema>
+      <BotaoTema onClick={props.trocaTema}>Trocar tema</BotaoTema>
     </SidebarContainer>
   </aside>
 )
